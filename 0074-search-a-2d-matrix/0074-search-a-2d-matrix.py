f9e2d010e56ba72025f61d7e18547c017597e071
Deepak Harish <harishdeepak35@gmail.com>
2026-07-07 11:58:01 +0530

    Implement searchMatrix method for target search
    Added a new searchMatrix method to the Solution class that iterates through the matrix to find the target.

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -15,4 +15,15 @@ class Solution(object):
             else:
                 high=mid-1
         return False
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n=len(matrix)
+        m=len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j]==target:
+                    return True
+        return False
         
