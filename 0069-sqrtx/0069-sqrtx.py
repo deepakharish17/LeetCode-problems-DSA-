8bcdbc2a34d9e5ad7c6a0b4730e1ea86666a69aa
Deepak Harish <harishdeepak35@gmail.com>
2026-07-07 12:17:54 +0530

    Time: 3 ms (55.26%), Space: 12.5 MB (17.18%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
         if x==0:
             return 0
         low , high = 1,x
