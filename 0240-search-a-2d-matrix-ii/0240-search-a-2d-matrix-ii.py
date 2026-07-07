19a44e8acb35e97a2ad9e711b1cf1c7f23beba60
Deepak Harish <harishdeepak35@gmail.com>
2026-07-07 12:03:33 +0530

    Time: 134 ms (38.88%), Space: 18.6 MB (13.01%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,25 +1,28 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        # n = len(matrix)
-        # m = len(matrix[0])
-        # low = 0
-        # high = n * m - 1
-        # while low<=high:
-        #     mid=(low + high)// 2
-        #     row = mid // m
-        #     col = mid % m
-        #     if matrix[row][col]==target:
-        #         return True
-        #     elif target>matrix[row][col]:
-        #         low=mid+1
-        #     else:
-        #         high=mid-1
-        # return False
-       
         n=len(matrix)
         m=len(matrix[0])
         for i in range(n):
             for j in range(m):
                 if matrix[i][j]==target:
                     return True
+        return False
+# class Solution:
+    def searchMatrix(self, matrix, target):
+        if not matrix or not matrix[0]:
+            return False
+        
+        m, n = len(matrix), len(matrix[0])
+        
+
+        row, col = 0, n - 1
+        
+        while row < m and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                col -= 1  
+            else:
+                row += 1  
+        
         return False
