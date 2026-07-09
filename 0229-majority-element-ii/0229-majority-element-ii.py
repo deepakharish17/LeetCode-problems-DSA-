ffc867f7da19b2935808e89621ec65a4952cf3a7
Deepak Harish <harishdeepak35@gmail.com>
2026-07-09 12:12:10 +0530

    Time: 15 ms (34.59%), Space: 15 MB (40.07%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,3 +1,12 @@
 class Solution:
     def majorityElement(self, nums):
-        return [num for num, count in Counter(nums).items() if count > len(nums) // 3]
+        # return [num for num, count in Counter(nums).items() if count > len(nums) // 3]
+
+        freq={}
+        for i in nums:
+            freq[i]=freq.get(i,0)+1
+        res=[]
+        for i in freq:
+            if freq[i]>len(nums)//3:
+               res.append(i)
+        return res
