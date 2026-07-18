83656425a258c69b6a7e76fc169072d9c03c64d1
Deepak Harish <harishdeepak35@gmail.com>
2026-07-18 10:46:20 +0530

    Time: 103 ms (91.17%), Space: 38 MB (53.55%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,30 +6,39 @@
 
 class Solution:
 
-    def getdifference(self,headA, headB):
-        len1,len2=0,0
-        while headA or headB:
-            while headA:
-                len1+=1
-                headA=headA.next
-            while headB:
-                len2+=1
-                headB=headB.next
-        return len1-len2
+    # def getdifference(self,headA, headB):
+    #     len1,len2=0,0
+    #     while headA or headB:
+    #         while headA:
+    #             len1+=1
+    #             headA=headA.next
+    #         while headB:
+    #             len2+=1
+    #             headB=headB.next
+    #     return len1-len2
     
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        diff = self.getdifference(headA,headB)
+        lista = headA
+        listb = headB
+        while lista != listb:
+            lista = lista.next if lista else headB
+            listb = listb.next if listb else headA
+        return listb
+    #     diff = self.getdifference(headA,headB)
 
-        if diff < 0:
-            while diff !=0:
-                headB=headB.next
-                diff+=1
-        else:
-            while diff!=0:
-                headA=headA.next
-                diff-=1
-        while headA != headB:
-            headA = headA.next
-            headB = headB.next
+    #     if diff < 0:
+    #         while diff !=0:
+    #             headB=headB.next
+    #             diff+=1
+    #     else:
+    #         while diff!=0:
+    #             headA=headA.next
+    #             diff-=1
+    #     while headA != headB:
+    #         headA = headA.next
+    #         headB = headB.next
             
-        return headA
+    #     return headA
+
+
+       
