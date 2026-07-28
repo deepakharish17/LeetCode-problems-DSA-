c94a8c4d968fc1433d48996d66076f3dce023c12
Deepak Harish <harishdeepak35@gmail.com>
2026-07-28 10:28:57 +0530

    Time: 124 ms (74.02%), Space: 83.2 MB (44.2%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+    # Create a stack/list and create and set a summy pointer with head
+    # add all values in stack
+    # now check all stack values is palindrome for that uses temp.val != stack[-1]
+    # and traverse the stack and figure it out
+        stack=[]
+        temp=head
+        while temp is not None:
+            stack.append(temp.val)
+            temp=temp.next
+        temp=head
+        while temp is not None:
+            if temp.val != stack[-1]:
+                return False
+            stack.pop()
+            temp=temp.next
+        return True
+
+
+
