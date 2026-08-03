c51fcac79cf08f044e9cb35b625f76b91c620f65
Deepak Harish <harishdeepak35@gmail.com>
2026-08-03 15:53:04 +0530

    Time: 40 ms (69.66%), Space: 29.2 MB (7.11%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -7,3 +7,19 @@ class Solution:
                 buy_price=i
             profit=max(profit,i-buy_price)
         return profit
+
+    # class Solution(object):
+    # def maxProfit(self, prices):
+    #     """
+    #     :type prices: List[int]
+    #     :rtype: int
+    #     """
+    #     buyPrice=prices[0]
+    #     profit=0
+    #     for i in range(1,len(prices)):
+    #         if buyPrice>prices[i]:
+    #             buyPrice=prices[i]
+    #         else:
+    #             profit=max(profit,prices[i]-buyPrice)
+    #     return profit
+        
