515afd0237fa08762bcbc4922dd1f7789d45e7b1
Deepak Harish <harishdeepak35@gmail.com>
2026-08-12 21:39:22 +0530

    Time: 20 ms (99.06%), Space: 12.3 MB (63.73%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -30,30 +30,24 @@ class Solution(object):
             for j in range(i + 1, n - 2):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
-                    
                 if nums[i] + nums[j] + nums[j+1] + nums[j+2] > target:
                     break
                 if nums[i] + nums[j] + nums[n-2] + nums[n-1] < target:
                     continue
-                    
                 left, right = j + 1, n - 1
                 
                 while left < right:
                     total = nums[i] + nums[j] + nums[left] + nums[right]
-                    
                     if total == target:
                         res.append([nums[i], nums[j], nums[left], nums[right]])
                         left += 1
                         right -= 1
-                        
                         while left < right and nums[left] == nums[left - 1]:
                             left += 1
                         while left < right and nums[right] == nums[right + 1]:
-                            right -= 1
-                            
+                            right -= 1  
                     elif total < target:
                         left += 1
                     else:
-                        right -= 1
-                        
+                        right -= 1  
         return res
