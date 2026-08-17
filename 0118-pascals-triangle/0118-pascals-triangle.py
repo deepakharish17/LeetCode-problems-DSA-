7345729988a9e0cf40e940749976e142d244cc0c
Deepak Harish <harishdeepak35@gmail.com>
2026-08-17 11:06:10 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.78%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,13 +1,9 @@
-class Solution(object):
-    def generate(self, numRows):
-        if numRows == 0:
-            return []
-            
-        triangle = []
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle=[]
         for i in range(numRows):
-            row = [1] * (i + 1)
-            for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row=[1]*(i+1)
+            for j in range(1,i):
+                row[j]=triangle[i-1][j-1] + triangle[i-1][j]
             triangle.append(row)
-            
         return triangle
