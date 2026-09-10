b528d2d0fa7dac5155da935b13e8e0c02dee9619
Deepak Harish <harishdeepak35@gmail.com>
2026-09-10 21:14:39 +0530

    Time: 0 ms (100%), Space: 19.2 MB (70.86%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -10,7 +10,7 @@ class Solution:
             f2=int(s[i-2:i])
             
             if 1<=f1<=9:
-                dp[i]=dp[i]+dp[i-1]
+                dp[i]+=dp[i-1]
             if 10<=f2<=26:
-                dp[i]=dp[i]+dp[i-2]
+                dp[i]+=dp[i-2]
         return dp[n]
